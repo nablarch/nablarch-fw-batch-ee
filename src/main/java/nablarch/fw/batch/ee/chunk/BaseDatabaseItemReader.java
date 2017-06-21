23a3c9284bb1a4386ae23a91d2ed18ddd231c5d7
yama-gen <naoki.aoi.y@gmail.com>
2017-06-21 15:33:00 +0900

    データベース接続を必要とするリーダは、ステップに紐付けられているデータベース接続と異なるものを使用するように変更
    NAB-157

---
+++ src/main/java/nablarch/fw/batch/ee/chunk/BaseDatabaseItemReader.java
@@ -0,0 +1,69 @@
+package nablarch.fw.batch.ee.chunk;
+
+import java.io.Serializable;
+
+import javax.batch.api.chunk.AbstractItemReader;
+
+import nablarch.core.db.connection.AppDbConnection;
+import nablarch.core.db.connection.ConnectionFactory;
+import nablarch.core.db.connection.DbConnectionContext;
+import nablarch.core.db.connection.TransactionManagerConnection;
+import nablarch.core.repository.SystemRepository;
+import nablarch.core.transaction.TransactionContext;
+
+/**
+ * データベースを入力とする{@link javax.batch.api.chunk.ItemReader}の抽象クラス。
+ * <p/>
+ * 本リーダを継承することで、リーダ専用のコネクションを使用してデータを読み込むことができる。
+ * <p/>
+ * DB製品によっては、トランザクション制御時にカーソルが閉じられてしまうため、リーダ専用のコネクションを使用して読み込みを行っている。
+ *
+ * @author Naoki Yamamoto
+ */
+public abstract class BaseDatabaseItemReader extends AbstractItemReader {
+
+    /** リーダ専用のコネクション */
+    private TransactionManagerConnection connection;
+
+    @Override
+    public final void open(final Serializable checkpoint) throws Exception {
+        
+        // 元々設定されていたデフォルトのデータベース接続を退避する
+        AppDbConnection originalConnection = null;
+        if (DbConnectionContext.containConnection(TransactionContext.DEFAULT_TRANSACTION_CONTEXT_KEY)) {
+            originalConnection = DbConnectionContext.getConnection();
+            DbConnectionContext.removeConnection();
+        }
+        try {
+            // リーダのopen用に新しい接続をデフォルトの接続とする。
+            final ConnectionFactory connectionFactory = SystemRepository.get("connectionFactory");
+            if (connectionFactory == null) {
+                throw new IllegalStateException("ConnectionFactory was not found." 
+                        + " must be set ConnectionFactory(component name=connectionFactory).");
+            }
+            connection = connectionFactory.getConnection(TransactionContext.DEFAULT_TRANSACTION_CONTEXT_KEY);
+            DbConnectionContext.setConnection(connection);
+            doOpen(checkpoint);
+        } finally {
+            // 退避した接続を元に戻す
+            if (originalConnection != null) {
+                DbConnectionContext.removeConnection();
+                DbConnectionContext.setConnection(originalConnection);
+            }
+        }
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (connection != null) {
+            connection.terminate();
+        }
+    }
+
+    /**
+     * データベースからのデータ読み込みを行う。
+     * @param checkpoint チェックポイント
+     * @throws Exception 発生した例外
+     */
+    protected abstract void doOpen(final Serializable checkpoint) throws Exception;
+}
